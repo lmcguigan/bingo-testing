208eef4d891a2f7273a22d267b93cdaa59223450
Lucy M <lmcguigan@users.noreply.github.com>
2020-05-19 17:24:32 -0500

    additional tests

--- src/utils/number-picker-utils.test.js
+++ src/utils/number-picker-utils.test.js
@@ -1,9 +1,23 @@
-import { getRandomNumber } from "./number-picker-utils"
+import { getRandomNumber, buildArrayOfRandomNumbers } from "./number-picker-utils"
 
 describe('Testing the method that generates a random number', () => {
     it('should return a number that is greater than 1 and less than or equal to 75', () => {
         const number = getRandomNumber()
         expect(number).toBeGreaterThan(0)
         expect(number).toBeLessThan(76)
+    })
+})
+describe('Testing the method that builds an array of random numbers', () => {
+    const arrayFiveByFive = buildArrayOfRandomNumbers(5)
+    const arrayThreeByThree = buildArrayOfRandomNumbers(3)
+    it('should return an array with length of the argument squared ', () => {
+        expect(arrayFiveByFive).toHaveLength(25)
+        expect(arrayThreeByThree).toHaveLength(9)
+    })
+    it('should be comprised of numbers that are each between 1 and 75', () => {
+        expect(arrayFiveByFive[13]).toBeGreaterThan(0)
+        expect(arrayFiveByFive.every(number => number > 0)).toBeTruthy()
+        expect(arrayFiveByFive[8]).toBeLessThan(75)
+        expect(arrayFiveByFive.every(number => number < 76)).toBeTruthy()
     })
 })
